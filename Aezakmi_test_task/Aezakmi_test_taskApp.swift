e9e3adbfdf6dedd13d707ccc055a9b5b74f4a217
rustemd02 <rustemd02@mail.ru>
2024-12-05 15:57:00 +0300

    Initial Commit

---
+++ Aezakmi_test_task/Aezakmi_test_taskApp.swift
@@ -0,0 +1,20 @@
+//
+//  Aezakmi_test_taskApp.swift
+//  Aezakmi_test_task
+//
+//  Created by Рустем on 05.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Aezakmi_test_taskApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
